afa37de54fc69ebaa88a4ed7c27f85055c46c95c
Claude Petit <doodadjs@gmail.com>
2016-11-15 21:43:22 -0500

    Add base url support

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 {
 
-  "host": "0.0.0.0",
-  "port": 7777,
+  "host": "127.0.0.1",
+  "port": 20485,
 
   "keyLength": 10,
 
@@ -33,10 +33,10 @@
   },
 
   "storage": {
-    "type": "memcached",
+    "type": "redis",
     "host": "127.0.0.1",
-    "port": 11211,
-    "expire": 2592000
+    "port": 6379,
+    "db": 1
   },
 
   "documents": {
